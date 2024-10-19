1b70f853d0205961154e7d41debd0a812a167cb0
agouy <alexandre.gouy@algolife.fr>
2024-10-19 16:03:46 +0200

    Built site for gh-pages

--- app/1_get_cadastre_shapefile.R
+++
@@ -1,9 +0,0 @@
-##### 
-## Get cadastre
-
-url_parcelles <- "https://cadastre.data.gouv.fr/data/etalab-cadastre/2024-04-01/geojson/communes/87/87153/cadastre-87153-parcelles.json.gz"
-data_folder <- "data"
-destfile <- file.path(data_folder, basename(url_parcelles))
-download.file(url_parcelles, destfile)
-R.utils::gunzip(destfile)
-
